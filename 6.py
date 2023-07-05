d472498c20c996747748da3cedf1ee16feef3e42
vamsi <vamsip094@gmail.com>
2023-07-05 22:42:10 +0530

    Added Day 4 Programs

---
+++ 6.py
@@ -0,0 +1,2 @@
+# Extend task 4, get the lengths/breadths of the two classes in command line arguments, validate the inputs
+# and perform the task as mentioned in task 4. Write the necessary docstrings.
